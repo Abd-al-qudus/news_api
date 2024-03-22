412216e0917ea0f2459e108d463712cf1fdd8c03
Abd-al-qudus <jimohqudus86@gmail.com>
2024-03-22 17:22:23 +0100

    set server strings

--- news_api/settings.py
+++ news_api/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'news_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'engineerphoenix$newsapp',
+        'USER': 'engineerphoenix',
+        'PASSWORD': 'newsapppwd',
+        'HOST': 'engineerphoenix.mysql.pythonanywhere-services.com',
     }
 }
 
